9619fc03d51b67f7b466153ad29e84f5b3166c85
w4termlon <w4termlon@gmail.com>
2023-05-05 14:02:37 +0200

    feat: updated alignement of combatants

--- src/components/CombatantList/CombatantView.tsx
+++ src/components/CombatantList/CombatantView.tsx
@@ -4,7 +4,7 @@ import { Box, Container, Typography } from "@suid/material";
 
 const CombatantView: Component<{ combatant: Combatant }> = (props) => {
     return (
-        <Container sx={{ fontWeight: 'bold', minWidth: '8rem', width: '13rem', margin: "0 0 0 0 !important", padding: "0 0 0 1%  !important" }} >
+        <Container sx={{ fontWeight: 'bold', minWidth: '149px', width: '10%', margin: "0 5px 0 5px !important", padding: "0 0 0 0 !important", maxWidth: "0" }} >
             <Box class="ticker" paddingLeft={"0.65rem"} flexDirection="row" justifyContent="space-between">
                 <Typography text-shadow=" 0 0 8px #000" whiteSpace="nowrap" overflow="hidden" textOverflow="clip" maxWidth={"9rem"} class={"icon class-" + props.combatant.Job.toLowerCase()}>
                     {props.combatant.name}
@@ -13,8 +13,8 @@ const CombatantView: Component<{ combatant: Combatant }> = (props) => {
                     {props.combatant.ENCDPS}
                 </Typography>
             </Box>
-            <Box zIndex={-1} top="0" position="fixed" minWidth={'136px'} maxWidth={'198px'} width={'12%'} height={'1.5rem'} class={"cell-class-" + props.combatant.Job.toLowerCase()} sx={{ transform: "skew(-30deg)", opacity: '0.6' }} />
-            <Box zIndex={-1} top="0" position="fixed" minWidth={'68px'} maxWidth={'99px'} width={'6%'} height={'1.5rem'} class={"cell-class-" + props.combatant.Job.toLowerCase()} sx={{ transform: "skew(-30deg)", opacity: '0.8' }} />
+            <Box zIndex={-1} top="0" position="fixed" minWidth={'153px'} maxWidth={'198px'} width={'10%'} height={'1.5rem'} class={"cell-class-" + props.combatant.Job.toLowerCase()} sx={{ transform: "skew(-30deg)", opacity: '0.6' }} />
+            <Box zIndex={-1} top="0" position="fixed" minWidth={'80px'} maxWidth={'99px'} width={'6%'} height={'1.5rem'} class={"cell-class-" + props.combatant.Job.toLowerCase()} sx={{ transform: "skew(-30deg)", opacity: '0.8' }} />
         </Container >
     )
 }
